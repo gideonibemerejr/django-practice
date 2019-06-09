454367648d1deb93efe0603a7a97caa6d64ca5eb
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-06-08 22:54:29 -0500

    Add static files and styles

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def index(request):
-    return render(request, 'articles/index.html')
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'articles/index.html', {'title': 'Articles', 'articles': articles})
